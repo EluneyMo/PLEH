d990272f04c4ad0673c67aa0a679a762b866e309
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-10 09:42:18 -0300

    Agregando label contraseña

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,7 +7,7 @@ const login = ()=> {
 <form action="/" className='form'>
 <label htmlFor="email" className="email">Ingrese email</label>
 <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
-
+<label htmlFor="contraseña" className="label">Contraseña</label>
 </form>
             </div>
 
