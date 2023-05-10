7ef200fc598a03d5c95930c63adf1284b72f19e6
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-10 09:38:38 -0300

    Creando label

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,9 +5,9 @@ const login = ()=> {
         <div className="login">
             <div className="logincontainter">
 <form action="/" className='form'>
+<label htmlFor="email" className="email">Ingrese email</label>
 
 
-    
 </form>
             </div>
 
