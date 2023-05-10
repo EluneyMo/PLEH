a60e0aeccc8dba4fe3a217c9dc6a3cd185eb858c
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-10 09:28:28 -0300

    Creando container de login

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const login = ()=> {
      return(
         <div className="login">
+            <div className="logincontainter">
 
-            
+            </div>
+
         </div>
      );
 }
