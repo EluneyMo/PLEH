f89c858ffbae6821bbf3bd08bfe9e72515800174
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-10 09:45:18 -0300

    Agregando input contraseña

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,7 +7,8 @@ const login = ()=> {
 <form action="/" className='form'>
 <label htmlFor="email" className="email">Ingrese email</label>
 <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
-<label htmlFor="contraseña" className="label">Contraseña</label>
+<label htmlFor="contraseña" className="contraseña">Contraseña</label>
+<input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" />
 </form>
             </div>
 
