a33fd58c402b89e026ba0cfd43e459373b38c277
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-11 18:44:49 -0300

    Cambiando nombre de constante

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from 'react';
 
-const login = ()=> {
+const Login = ()=> {
      return(
         <div className="login">
             <div className="logincontainter">
