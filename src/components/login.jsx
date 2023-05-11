1423dcf35213e2dad33e93046c40809600d97b8f
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-11 19:00:46 -0300

    Agregando salto de linea

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,10 +6,10 @@ const Login = ()=> {
             <div className="logincontainter">
                 <form action="/" className='form'>
                     <label htmlFor="email" className="email">Ingrese email</label><br/>
-                        <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
-                             <label htmlFor="contraseña" className="contraseña">Contraseña</label>
-                                 <input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" />
-                                    <input type="submit" value="Log in" className="loginboton" />
+                        <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" /><br/>
+                             <label htmlFor="contraseña" className="contraseña">Contraseña</label><br/>
+                                 <input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" /><br/>
+                                    <input type="submit" value="Log in" className="loginboton" /><br/>
                                         <a href="/">¿Olvidaste tu contraseña?</a>
                 </form>
                     <button className="signupboton">Registrarse</button>
