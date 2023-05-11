fdcc125299bf61486a317b3060652948ec237e69
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-11 18:19:30 -0300

    Tabulando

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -5,9 +5,9 @@ const login = ()=> {
      return(
         <div className="login">
             <div className="logincontainter">
-<form action="/" className='form'>
-<label htmlFor="email" className="email">Ingrese email</label>
-<input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
+                <form action="/" className='form'>
+                    <label htmlFor="email" className="email">Ingrese email</label>
+                    <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
 <label htmlFor="contraseña" className="contraseña">Contraseña</label>
 <input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" />
 <input type="submit" value="Log in" className="loginboton" />
