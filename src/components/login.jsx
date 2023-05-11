2a999a0c5997be15bb7a9b6ef3eb9d4f5e7c0f66
EluneyMo <morales.eluney.epet20@gmail.com>
2023-05-11 18:27:45 -0300

    Tabulando

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -7,13 +7,13 @@ const login = ()=> {
             <div className="logincontainter">
                 <form action="/" className='form'>
                     <label htmlFor="email" className="email">Ingrese email</label>
-                    <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
-<label htmlFor="contraseña" className="contraseña">Contraseña</label>
-<input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" />
-<input type="submit" value="Log in" className="loginboton" />
-<a href="/">¿Olvidaste tu contraseña?</a>
-</form>
-<button className="signupboton">Registrarse</button>
+                        <input type="text" id="email" placeholder="robin23232@example.com" className="inputemail" />
+                             <label htmlFor="contraseña" className="contraseña">Contraseña</label>
+                                 <input type="contraseña" id="contraseña" placeholder="*********" className="inputcontraseña" />
+                                    <input type="submit" value="Log in" className="loginboton" />
+                                        <a href="/">¿Olvidaste tu contraseña?</a>
+                </form>
+                    <button className="signupboton">Registrarse</button>
             </div>
 
         </div>
