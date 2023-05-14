c559ae553f7a4765a01adcd595ba0ef1b654397d
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-05-14 19:06:46 -0300

    agrego direcion al href del button button

--- src/components/RetrocederButton.js
+++ src/components/RetrocederButton.js
@@ -1,6 +1,8 @@
 import {} from 'react';
 function RetrocederButton() {
     return(
-        <button ></button>
+        <button  onclick ={()=> {window.location.href="/";}}>
+            <FontAwesomeIcon icon="fa-solid fa-arrow-rotate-left" />
+        </button>
     )
 }
