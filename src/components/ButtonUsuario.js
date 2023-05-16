0bf543de3228296478a221766ed626e3b2487fc4
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-05-16 16:13:19 -0300

    paso los compoentes de molina a la rama develop

---
+++ src/components/ButtonUsuario.js
@@ -0,0 +1,10 @@
+import {} from 'react';
+function UsuarioButton(){
+    return (
+    <button onclick ={()=> {window.location.href="/Usuario.js";}}>
+        <FontAwesomeIcon icon="fa-light fa-user" />" 
+    </button>  
+    )
+}
+
+export default UsuarioButton;
