c3147f07f0f6df82848205d76984761e286c7c8d
abril005 <abril.levillan.epet20@gmail.com>
2023-05-16 17:11:44 -0300

    Agregando la funcion a inputs

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,6 +1,22 @@
 function Inputs() {
-    return(
-        o
+    const handleFormSubmit = (event) => {
+      event.preventDefault();
+      // Aquí puedes agregar la lógica para enviar los datos del formulario
+    };
+  
+    return (
+      <form onSubmit={handleFormSubmit}>
+        <div>
+          <label htmlFor="email">Correo:</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Contraseña:</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
     );
-    
-    }
+  }
+  
+  export default Inputs;
