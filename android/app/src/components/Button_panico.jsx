91a4d433b690da03dcca5b1430f6b5d299fb2291
EluneyMo <morales.eluney.epet20@gmail.com>
2023-06-06 19:38:56 -0300

    Agregando todas las dependencias

---
+++ android/app/src/components/Button_panico.jsx
@@ -0,0 +1,7 @@
+function Button_Panico() {
+    return(
+        o
+    );
+    
+    
+    }
---
+++ android/app/src/components/Mapa.jsx
@@ -0,0 +1,8 @@
+import {useState, useEffect} from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+render (){
+    return(
+        <div id="map" style={{ width: '100%', height: '400px' }}></div>
+    )
+}
