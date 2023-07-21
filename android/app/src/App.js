14ce41eeb0ee32f35cbf4503b7fba63eb43fb8bb
EluneyMo <morales.eluney.epet20@gmail.com>
2023-07-20 21:11:13 -0300

    Reimportando librerias

--- android/app/src/App.js
+++ android/app/src/App.js
@@ -2,8 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import LoginComponent from "./components/Login.jsx";
-import HomeComponent from "./HomeComponent";
+import HomeComponent from "/components/HomeComponent.jsx";
 import Login from './components/Login.jsx';
 
 function App() {
