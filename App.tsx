75bd04e9593676bfd2d0156994d77e4d262f0a00
EluneyMo <morales.eluney.epet20@gmail.com>
2023-07-27 22:38:27 -0300

    Cambiando a texto

--- App.tsx
+++ App.tsx
@@ -4,9 +4,10 @@ import React from "react";
 function App() {
   return (
   <React.Fragment>
-
-    <text>HOOOOOOOOOOLAAAAAAA</text>
+    <div class="Appcontainer">
+    throw ("HOOOOOOOOOOLAAAAAAA");
    </React.Fragment>
+   </div>
   );
 }
 export default App;
