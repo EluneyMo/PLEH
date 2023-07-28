66492c21202253cf04e741edc5cdf9de7c651620
EluneyMo <morales.eluney.epet20@gmail.com>
2023-07-27 22:24:03 -0300

    Borrando todo

--- App.tsx
+++ App.tsx
@@ -1,2 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomeComponent from "android/app/src/components/HomeComponent.jsx";
+import Login from 'android/app/src/components/Login.jsx';
 
+function App() {
+  return (
+ <Router>
+ <Route exact path="android/app/src/components/" component={Login} />
+ <Route path="android/app/src/components/" component={HomeComponent} />
+</Router>
+    <h1>HOOOOOOOOOOLAAAAAAA</h1>
+  );
+}
 export default App;
