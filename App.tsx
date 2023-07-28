6ad3831a26cc060272f9a3f31f8aaeba57cf03c8
EluneyMo <morales.eluney.epet20@gmail.com>
2023-07-27 22:32:11 -0300

    Borrando cosas

--- App.tsx
+++ App.tsx
@@ -1,16 +1,10 @@
 
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import HomeComponent from "android/app/src/components/HomeComponent.jsx";
-import Login from 'android/app/src/components/Login.jsx';
 
 function App() {
   return (
   <React.Fragment>
-     <Router>
-         <Route exact path="android/app/src/components/" component={Login} />
-         <Route path="android/app/src/components/" component={HomeComponent} />
-    </Router>
+
     <h1>HOOOOOOOOOOLAAAAAAA</h1>
    </React.Fragment>
   );
