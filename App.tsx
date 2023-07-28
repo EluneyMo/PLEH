d1370920948a968e8a11d486f0d159f1d63a476f
EluneyMo <morales.eluney.epet20@gmail.com>
2023-07-27 22:29:22 -0300

    Borrando cosas

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import HomeComponent from "android/app/src/components/HomeComponent.jsx";
