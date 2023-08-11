63437698bfdd5bc01e7eeb05afc6f7bf8439f783
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-08-11 10:45:22 -0300

    agrego usuario creado  nuevo

--- src/components/AltaUsuario.js
+++ src/components/AltaUsuario.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-const SignupScreen = () => {
+const Sigup = () => {
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 }
+const usuariocreado = async () => {}
+export default Sigup
