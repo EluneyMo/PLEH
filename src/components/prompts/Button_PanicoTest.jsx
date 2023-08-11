b7b3ce518322868a51328f9910fd7d7e9482aa47
abril005 <abril.levillan.epet20@gmail.com>
2023-08-11 11:29:00 -0300

    agradando al div y button className

--- src/components/prompts/Button_PanicoTest.jsx
+++ src/components/prompts/Button_PanicoTest.jsx
@@ -4,10 +4,10 @@ import './Button_Panico.css';
 function BotonFlotante() {
 
     return (
-        <div >
+        <div className='contenedor'>
 
-            <button > 
-            <img src="" alt="" className="" />
+            <button className='botonFlotante'> 
+          //*   <div styleName="" /><img src="./" alt="boton" className="boton" />
             </button>
 
         </div>
