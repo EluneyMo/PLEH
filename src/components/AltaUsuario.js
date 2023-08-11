8e913c7756f130ad1c313311afaf4c0c7ddd4b12
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-08-11 10:41:52 -0300

    seteo la ala usuario

--- src/components/AltaUsuario.js
+++ src/components/AltaUsuario.js
@@ -1,4 +1,6 @@
 import React from "react";
 import { View, TextInput, Button, StyleSheet } from 'react-native';
 
-const SignupScreen = () => {}
+const SignupScreen = () => {
+    const [username, setUsername] = useState('');
+}
