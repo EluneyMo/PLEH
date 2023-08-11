a7c6ea805de20d750c3c8aa70767b5a0ccbc3477
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-08-11 10:34:29 -0300

    agrego un touchble

--- src/components/RetrocederButton.js
+++ src/components/RetrocederButton.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { FontAwesomeIcon } from '@expo/vector-icons'; 
 function RetrocederButton() {
-    return(
-
-        <button  onclick ={()=> {window.history.back();}}>
-            <FontAwesomeIcon icon="fa-solid fa-arrow-rotate-left" />
-        </button>
-    )
+    return (
+            <TouchableOpacity
+              onPress={() => {
+              }}
+              style={styles.UsuarioButton}
+            >
+              <FontAwesomeIcon name="user" />
+            </TouchableOpacity>
+          );
 }
 
 const styles = StyleSheet.create({
