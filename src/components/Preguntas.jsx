b2fba605421ea2736b42d79748aca089a643c23f
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-08-11 23:35:25 -0300

    agrego const para mostrar preguntas

--- src/components/Preguntas.jsx
+++ src/components/Preguntas.jsx
@@ -2,3 +2,7 @@ import React, { useState } from 'react';
 import botonlargo from './botonlargo';
 import cajapreguntas2 from './cajapreguntas2';
 import botonpanico from './botonpanico'
+
+const cajapreguntas2 = () => {
+    
+}
