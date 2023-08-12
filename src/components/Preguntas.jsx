32ddd00db8a1bf5d49470f79195cd211e10275f2
MOLINA-JUAN <103200078+MOLINA-JUAN@users.noreply.github.com>
2023-08-11 23:34:17 -0300

    arreglo los imports

--- src/components/Preguntas.jsx
+++ src/components/Preguntas.jsx
@@ -1,4 +1,4 @@
 import React, { useState } from 'react';
-import Header from './Header';
 import botonlargo from './botonlargo';
 import cajapreguntas2 from './cajapreguntas2';
+import botonpanico from './botonpanico'
